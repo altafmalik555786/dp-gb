fb8ab0dc9519ebae3a0ea479b6648c3edce4fb6c
Shoaib Amjad <shoaib@simplifyvms.com>
2022-09-21 23:21:34 +0500

    Added user model, simplejwt authentication, signin api

--- main_platform_backend/home/views.py
+++ main_platform_backend/home/views.py
@@ -1,6 +1,7 @@
 from rest_framework import viewsets
-from rest_framework.permissions import AllowAny
+from rest_framework.permissions import AllowAny, IsAuthenticated
 from rest_framework.response import Response
+from rest_framework.views import APIView
 
 from home.models import Listing
 from home.serializers import ListingSerializer
@@ -30,3 +31,10 @@ class SpView(viewsets.ViewSet):
             admin.save()
             return Response('ok')
         return Response('Stay Away')
+
+
+class ProtectedView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def get(self, req):
+        return Response({'message': 'hello!!!'})
